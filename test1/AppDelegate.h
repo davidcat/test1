6ce83d3eb4fc0a85677c45c79392487f952addfc
hanpanpan <hanpanpan@chushi007.com>
2015-03-12 14:27:45 +0800

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by admin on 15/3/12.
+//  Copyright (c) 2015年 Lekuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
